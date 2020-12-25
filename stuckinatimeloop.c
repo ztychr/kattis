90fcdf29ec00b54c0590e44d461dc958aa5eaa62
ztychr <nordskov@protonmail.com>
2020-12-25 23:02:21 +0100

    timeloop

---
+++ stuckinatimeloop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+
+    scanf("%d", &num);
+
+    for(int i = 1; i <= num; i++){
+        printf("%d Abracadabra\n", i);
+    }
+
+    return 0;
+}
